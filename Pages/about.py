dc94a3449d55cff8897a49a1d088cdc7f94190fa
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-01 17:17:32 +0700

    Update about.py

--- Pages/about.py
+++ Pages/about.py
@@ -1,1 +1,6 @@
+import streamlit as st
+
+st.title("About")
+st.write("This application allows users to submit and grade C++ code based on given problems.")
+st.write("You can select a problem, submit your code, and get feedback on your performance.")
 
