79239ed118e1ff7a667456efea2bc1690e614801
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-01 17:20:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from streamlit_option_menu import option_menu
 
 # Define the pages to navigate
 with st.sidebar:
-    Page = option_menu(
+    page = option_menu(
         "Navigation",
         ["Home", "Problems", "Submissions", "About"],
         icons=["house", "clipboard", "file-earmark-text", "info-circle"],
         default_index=0
     )
 
+# Page routing
 if page == "Home":
     import Pages.home
 elif page == "Problems":
