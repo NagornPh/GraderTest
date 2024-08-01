9ef5c252e4904a4301949fe388d3494759fc711f
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-01 17:27:28 +0700

    Update home.py

--- Pages/home.py
+++ Pages/home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("The New And Better PDS Grader: Home")
+st.title("The New And Better PDS Grader")
 st.write("Welcome to the Home page. (Content to be added later.)")
 
