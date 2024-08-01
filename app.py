9b881d476c2578517642f6e7dca6d7bd5318cf2c
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-01 17:20:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# Define the pages to navigate
+st.title("Test Navigation")
+
 with st.sidebar:
     page = option_menu(
-        "Navigation",
-        ["Home", "Problems", "Submissions", "About"],
-        icons=["house", "clipboard", "file-earmark-text", "info-circle"],
+        "Test",
+        ["Page 1", "Page 2"],
+        icons=["house", "clipboard"],
         default_index=0
     )
 
-# Page routing
-if page == "Home":
-    import Pages.home
-elif page == "Problems":
-    import Pages.problems
-elif page == "Submissions":
-    import Pages.submissions
-elif page == "About":
-    import Pages.about
+if page == "Page 1":
+    st.write("You are on Page 1")
+elif page == "Page 2":
+    st.write("You are on Page 2")
