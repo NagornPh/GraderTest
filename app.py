6356d8c2f39422fbdb8663e4564e4d12592fe776
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-22 22:37:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ st.set_page_config(page_title="notafaketrip.com")
 # Title of the website
 st.title("notafaketrip.com")
 
+st.download_button("Download Brochure", data=pdf.read(), file_name=f"brochure.pdf")
 st.write("Trip Itenerary Video: ")
 st.write("Extra video: https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx")
