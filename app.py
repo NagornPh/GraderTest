eae2f05a35fc6bcfece801a481003b133e710ab9
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-22 22:38:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ st.set_page_config(page_title="notafaketrip.com")
 st.title("notafaketrip.com")
 
 pdf_path = f"./tripfiles/brochure.pdf"
-    if os.path.exists(pdf_path):
-        with open(pdf_path, "rb") as pdf:
-            st.download_button("Download Brochure", data=pdf.read(), file_name="brochure.pdf")
+with open(pdf_path, "rb") as pdf:
+    st.download_button("Download Brochure", data=pdf.read(), file_name="brochure.pdf")
 st.write("Trip Itenerary Video: ")
 st.write("Extra video: https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx")
