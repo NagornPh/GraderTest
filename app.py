6c9a86455b23172b9b031baf8637773bc45680df
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-22 22:40:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ st.title("notafaketrip.com")
 
 pdf_path = f"./tripfiles/brochure.pdf"
 with open(pdf_path, "rb") as pdf:
-    st.download_button("Download Brochure", data=pdf.read(), file_name="brochure.pdf")
+    st.download_button("Download Tokyo Trip Brochure", data=pdf.read(), file_name="brochure.pdf")
+    
+pdf_path = f"./tripfiles/trip_details.pdf"
+with open(pdf_path, "rb") as pdf:
+    st.download_button("Download Tokyo Trip Details", data=pdf.read(), file_name="trip_details.pdf")
+    
 st.write("Trip Itenerary Video: ")
 st.write("Extra video: https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx")
