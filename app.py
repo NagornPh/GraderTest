b22df3a5c2039fe23aeac9d3eb3631ac64285566
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-22 20:05:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,29 +6,4 @@ st.set_page_config(page_title="notafaketrip.com")
 # Title of the website
 st.title("notafaketrip.com")
 
-# Menu - Sidebar
-menu = ["Home", "Trips"]
-choice = st.sidebar.selectbox("Menu", menu)
-
-if choice == "Trips":
-    st.subheader("Available Trips")
-    if st.button("Tokyo Deluxe Plan"):
-        # Buttons for Brochure, Trip Details, and Book
-        brochure = st.button("Brochure")
-        details = st.button("Trip Details")
-        book = st.button("Book")
-        
-        # Handle Brochure download
-        if brochure:
-            with open("files/brochure.pdf", "rb") as file:
-                st.download_button(label="Download Brochure", data=file, file_name="brochure.pdf")
-
-        # Handle Trip Details download
-        if details:
-            with open("files/trip_details.pdf", "rb") as file:
-                st.download_button(label="Download Trip Details", data=file, file_name="trip_details.pdf")
-        
-        # Handle Book action
-        if book:
-            st.subheader("To be continued...")
-
+st.write("https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx")
