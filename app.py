d0af225e5f11eb29ed66d9d283688229c0559900
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-08-22 20:15:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ st.set_page_config(page_title="notafaketrip.com")
 # Title of the website
 st.title("notafaketrip.com")
 
-st.write("https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx")
+st.write("Trip Itenerary Video: ")
+st.write("Extra video: https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx")
